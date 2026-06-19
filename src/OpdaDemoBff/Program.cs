c0231a85ff35fc08de691f3dc8e1992b5fb30489
Tris <TechSupport@openpropdata.org.uk>
2026-06-19 06:07:45 +0100

    back to hard coded because we can't have nice things

--- src/OpdaDemoBff/Program.cs
+++ src/OpdaDemoBff/Program.cs
@@ -154,12 +154,11 @@ app.MapGet("/demo-api/pack/{uprn}", async (
     var paddedUprn = PadUprn(uprn);
     var tn = string.IsNullOrEmpty(titleNumber) ? "EXC10010" : titleNumber;
 
-    var msgId = Random.Shared.Next(100000, 999999);
     var lrBody = new
     {
-        messageId = msgId.ToString(),
-        externalReference = $"ext-{msgId}",
-        customerReference = $"cust-{paddedUprn}",
+        messageId = "170100",
+        externalReference = "extRef0001",
+        customerReference = "custRef0001",
         titleNumber = tn,
         expectedPrice = 10,
         titleKnownOfficialCopy = new
